7b106edb9c2bb068e862bba3b37eb9440d275492
E053689 <nathan.adam@bbvaitspain.com>
2019-03-23 22:30:28 +0100

    Testing pre-commit hook

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 const moviesRef = firebase.database().ref("peliculas");
-const apiKey = "😉😉TU TOKEN"
+const apiKey = "😉😉TU TOKEN";
 
-function addMovie (data){
-    return moviesRef.push(data)
+function addMovie(data) {
+  return moviesRef.push(data);
 }
 
-function deleteMovie (id){
-    return moviesRef.child(id).remove()
+function deleteMovie(id) {
+  return moviesRef.child(id).remove();
 }
 
-function updateMovie (id, data){
-    return moviesRef.child(id).set(data)
+function updateMovie(id, data) {
+  return moviesRef.child(id).set(data);
 }
 
 /**
  * getMovieDetails - retrieves the movie details from the data base by ID
  * @param {String} id - the movie ID
  * @return {Object} The movie deatils if ID exists in DB, an empty object if it
- * does not 
+ * does not
  */
-async function getMovieDetails (id) {
+async function getMovieDetails(id) {
   const movieDetails = await moviesRef.child(id).once("value");
-  return movieDetails && movieDetails.val() || {};
+  return (movieDetails && movieDetails.val()) || {};
 }
 
-function getMovieData (title) {
-    const url = `http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`
-    return fetch(url).then(res => res.json())
+function getMovieData(title) {
+  const url = `http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`;
+  return fetch(url).then(res => res.json());
 }
 
-function showDetails (data){
-    detailsSlctr.style.display = "block";
-    detailsSlctr.innerHTML = `<pre><code>${JSON.stringify(data, null, 4)}</code></pre>`;
-
+function showDetails(data) {
+  detailsSlctr.style.display = "block";
+  detailsSlctr.innerHTML = `<pre><code>${JSON.stringify(
+    data,
+    null,
+    4
+  )}</code></pre>`;
 }
 
 const filmSlctr = document.getElementById("peliculas");
@@ -42,10 +45,11 @@ const detailsSlctr = document.getElementById("details");
 //Eventos
 
 moviesRef.on("value", data => {
-    const peliculasData = data.val()
-    console.log("data:", peliculasData)
+  const peliculasData = data.val();
+  console.log("data:", peliculasData);
 
-    let htmlFinal = Object.keys(peliculasData).map(peliculaId => {
+  let htmlFinal = Object.keys(peliculasData)
+    .map(peliculaId => {
       const element = peliculasData[peliculaId];
       return `
         <li data-id="${peliculaId}">${element.Title}
@@ -54,40 +58,38 @@ moviesRef.on("value", data => {
           <button data-action="delete">Borrar</button>
         </li>
       `;
-    }).join('');
+    })
+    .join("");
 
-    filmSlctr.innerHTML = htmlFinal
-})
-
+  filmSlctr.innerHTML = htmlFinal;
+});
 
 filmSlctr.addEventListener("click", event => {
-    const target = event.target;
-    if(target.nodeName === "BUTTON") {
-        const id = target.parentNode.dataset.id;
-        const action = target.dataset.action;
-        if(action === "details") {
-            getMovieDetails(id)
-                .then(showDetails);
-        } else if (action === "edit") {
-            const newTitle = prompt("Dime el nuevo titulo").trim();
-            if(newTitle){
-                getMovieData(newTitle)
-                .then(movieDetails => updateMovie(id, movieDetails))
-            }
-        } else if (action === "delete") {
-            if(confirm("Estas seguro?")){
-                deleteMovie(id)
-            }
-        }
+  const target = event.target;
+  if (target.nodeName === "BUTTON") {
+    const id = target.parentNode.dataset.id;
+    const action = target.dataset.action;
+    if (action === "details") {
+      getMovieDetails(id).then(showDetails);
+    } else if (action === "edit") {
+      const newTitle = prompt("Dime el nuevo titulo").trim();
+      if (newTitle) {
+        getMovieData(newTitle).then(movieDetails =>
+          updateMovie(id, movieDetails)
+        );
+      }
+    } else if (action === "delete") {
+      if (confirm("Estas seguro?")) {
+        deleteMovie(id);
+      }
     }
-})
+  }
+});
 
 titleSlctr.addEventListener("keyup", event => {
-    const titleContent = titleSlctr.value.trim();
-    if(event.keyCode === 13 && titleContent){
-        console.log("ahora si!", titleContent)
-        getMovieData(titleContent)
-        .then(addMovie)
-    }
-})
-
+  const titleContent = titleSlctr.value.trim();
+  if (event.keyCode === 13 && titleContent) {
+    console.log("ahora si!", titleContent);
+    getMovieData(titleContent).then(addMovie);
+  }
+});
