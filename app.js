61f6017e4fde1aa33a70e9798a70221565ef1cfd
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2019-03-23 19:37:49 +0100

    Gestion del Input y listado basico implementado #4

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ function updateMovie (id, data){
 }
 
 function getMovieDetails (id) {
+    //@TODO: Refactor por la comunidad
     return new Promise ((resolve, reject) => {
         moviesRef.child(id).once("value", data => {
             resolve(data.val())
@@ -21,15 +22,39 @@ function getMovieDetails (id) {
     })
 }
 
-// @TODO: Refactor para trabajar con UI directamente
-function getMovies () {
-    moviesRef.on("value", data => {
-        console.log("data:", data.val())
-    })
-}
-
 function getMovieData (title) {
     const url = `http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`
     return fetch(url).then(res => res.json())
 }
+
+
+const filmSlctr = document.getElementById("peliculas");
+const titleSlctr = document.getElementById("title");
+
+//Eventos
+
+moviesRef.on("value", data => {
+    const peliculasData = data.val()
+    console.log("data:", peliculasData)
+
+    let htmlFinal = "";
+    //@TODO: Refactor por la comunidad, usando Arrays :-)
+    for (const key in peliculasData) {
+        if (peliculasData.hasOwnProperty(key)) {
+            const element = peliculasData[key];
+            htmlFinal += `<li>${element.Title}</li>`;    
+        }
+    }
+    filmSlctr.innerHTML = htmlFinal
+})
+
+titleSlctr.addEventListener("keyup", event => {
+    const titleContent = titleSlctr.value.trim();
+    if(event.keyCode === 13 && titleContent){
+        console.log("ahora si!", titleContent)
+        getMovieData(titleContent)
+        .then(addMovie)
+    }
+
+})
 
